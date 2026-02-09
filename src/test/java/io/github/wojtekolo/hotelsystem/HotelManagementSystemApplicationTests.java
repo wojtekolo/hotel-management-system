ee0b812c545bc331d49f2ec99a5e84f9046fc42f
wojtekolo <wojciechk531@gmail.com>
2026-02-09 16:45:20 +0100

    Initial commit

---
+++ src/test/java/io/github/wojtekolo/hotelsystem/HotelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wojtekolo.hotelsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
