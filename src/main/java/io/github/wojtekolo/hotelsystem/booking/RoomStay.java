bebe870e35d4a8857d37b718a94ef8b2f1c1e079
wojtekolo <wojciechk531@gmail.com>
2026-02-23 12:03:57 +0100

    feat: add stay modification methods to RoomStay

--- src/main/java/io/github/wojtekolo/hotelsystem/booking/RoomStay.java
+++ src/main/java/io/github/wojtekolo/hotelsystem/booking/RoomStay.java
@@ -1,6 +1,5 @@
 package io.github.wojtekolo.hotelsystem.booking;
 
-import io.github.wojtekolo.hotelsystem.customer.Customer;
 import io.github.wojtekolo.hotelsystem.employee.Employee;
 import io.github.wojtekolo.hotelsystem.guest.Guest;
 import io.github.wojtekolo.hotelsystem.room.Room;
@@ -75,13 +74,13 @@ public class RoomStay {
     private Employee checkOutBy;
 
     @PrePersist
-    private void setCreateTime(){
+    private void setCreateTime() {
         createTime = LocalDateTime.now();
         lastUpdateTime = LocalDateTime.now();
     }
 
     @PreUpdate
-    private void setUpdateTime(){
+    private void setUpdateTime() {
         this.lastUpdateTime = LocalDateTime.now();
     }
 
@@ -90,18 +89,82 @@ public class RoomStay {
         return pricePerNight.multiply(BigDecimal.valueOf(days));
     }
 
-    public static RoomStay createPlanned(Booking booking, Room room, BigDecimal discount, Employee employee, LocalDate from, LocalDate to, BigDecimal customPricePerNight) {
+    public boolean canBeCancelled() {
+        return status == RoomStayStatus.CANCELLED || status == RoomStayStatus.PLANNED;
+    }
+
+    public boolean canEditPrice() {
+        return status == RoomStayStatus.PLANNED;
+    }
+
+    public boolean canEditStartDate() {
+        return status == RoomStayStatus.PLANNED;
+    }
+
+    public boolean canEditEndDate() {
+        return status == RoomStayStatus.PLANNED || status == RoomStayStatus.ACTIVE;
+    }
+
+    public boolean doesCollide(){
+        return status == RoomStayStatus.PLANNED || status == RoomStayStatus.ACTIVE;
+    }
+
+    public boolean canEditRoom() {
+        return status == RoomStayStatus.PLANNED;
+    }
+
+    public static RoomStay create(Booking booking, Room room, BigDecimal discount, Employee employee, LocalDate from, LocalDate to, BigDecimal customPricePerNight, RoomStayStatus status) {
         return RoomStay.builder()
                        .booking(booking)
                        .room(room)
                        .pricePerNight(calculatePricePerNight(room, discount, customPricePerNight))
                        .activeFrom(from)
                        .activeTo(to)
-                       .status(RoomStayStatus.PLANNED)
+                       .status(status)
                        .createBy(employee)
                        .build();
     }
 
+    public boolean tryCancel() {
+        if (canBeCancelled()) {
+            this.status = RoomStayStatus.CANCELLED;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean tryUpdateActiveFrom(LocalDate newActiveFrom) {
+        if (canEditStartDate() || newActiveFrom.equals(this.activeFrom)) {
+            this.activeFrom = newActiveFrom;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean tryUpdateActiveTo(LocalDate newActiveTo) {
+        if (canEditEndDate() || newActiveTo.equals(this.activeTo)) {
+            this.activeTo = newActiveTo;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean tryEditPrice(BigDecimal newPricePerNight) {
+        if (newPricePerNight == null) return true;
+        if (canEditPrice() || newPricePerNight.equals(this.pricePerNight)) {
+            this.pricePerNight = newPricePerNight;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean tryUpdateRoom(Room room) {
+        if (canEditRoom() || room.getId().equals(this.room.getId())) {
+            this.room = room;
+            return true;
+        }
+        return false;
+    }
     private static BigDecimal calculatePricePerNight(Room room, BigDecimal discount, BigDecimal customPricePerNight) {
         if (customPricePerNight == null) {
             return room.getType().getPricePerNight()
