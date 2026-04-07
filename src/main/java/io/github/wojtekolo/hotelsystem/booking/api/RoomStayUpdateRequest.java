2d2da674e048826e3edcb46bd13e849396e54e3e
wojtekolo <wojciechk531@gmail.com>
2026-04-07 12:30:16 +0200

    fix: validate `Booking` update and create requests

--- src/main/java/io/github/wojtekolo/hotelsystem/booking/api/RoomStayUpdateRequest.java
+++ src/main/java/io/github/wojtekolo/hotelsystem/booking/api/RoomStayUpdateRequest.java
@@ -1,12 +1,18 @@
 package io.github.wojtekolo.hotelsystem.booking.api;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record RoomStayUpdateRequest(
+        @NotNull(message = "Room Stay ID is required")
         Long id,
+        @NotNull(message = "Room ID is required")
         Long roomId,
+        @NotNull(message = "Start date is required")
         LocalDate from,
+        @NotNull(message = "End date is required")
         LocalDate to,
         BigDecimal pricePerNight
 ) {
