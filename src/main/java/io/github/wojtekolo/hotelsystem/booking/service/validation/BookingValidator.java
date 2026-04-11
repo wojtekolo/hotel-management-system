63ee42144becbe96fdd6ee9566699c973297d337
wojtekolo <wojciechk531@gmail.com>
2026-04-12 00:09:59 +0200

    fix: don't verify RoomStay if dates are not in order

--- src/main/java/io/github/wojtekolo/hotelsystem/booking/service/validation/BookingValidator.java
+++ src/main/java/io/github/wojtekolo/hotelsystem/booking/service/validation/BookingValidator.java
@@ -1,7 +1,10 @@
 package io.github.wojtekolo.hotelsystem.booking.service.validation;
 import io.github.wojtekolo.hotelsystem.booking.model.entity.Booking;
+import io.github.wojtekolo.hotelsystem.booking.model.entity.RoomStay;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -11,9 +14,11 @@ public class BookingValidator {
     private final BookingExternalValidator externalValidator;
 
     public BookingValidationResult validateBooking(Booking booking) {
+        List<RoomStay> staysToValidate = booking.getStays().stream()
+                .filter(stay -> stay.getActiveTo().isAfter(stay.getActiveFrom())).toList();
         return new BookingValidationResult(
-                externalValidator.validate(booking.getStays(), booking.getId()),
-                internalValidator.validate(booking.getStays())
+                externalValidator.validate(staysToValidate, booking.getId()),
+                internalValidator.validate(staysToValidate)
         );
     }
 }
