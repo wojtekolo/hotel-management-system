a2dfc6d45f6c61a952f8be2742c8b84887d0f863
wojtekolo <wojciechk531@gmail.com>
2026-04-26 20:23:37 +0200

    feat: check if Redis enabled before creating Redis Beans

--- src/main/java/io/github/wojtekolo/hotelsystem/common/config/redis/RedisConfig.java
+++ src/main/java/io/github/wojtekolo/hotelsystem/common/config/redis/RedisConfig.java
@@ -1,5 +1,6 @@
 package io.github.wojtekolo.hotelsystem.common.config.redis;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.serializer.*;
 import java.time.Duration;
 
 @Configuration
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
 public class RedisConfig {
 
     @Bean
