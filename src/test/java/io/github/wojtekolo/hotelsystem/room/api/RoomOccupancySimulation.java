dc6174ac9e1ed1fc358eac59c6884b2b91f21774
wojtekolo <154454428+wojtekolo@users.noreply.github.com>
2026-06-02 19:30:50 +0200

    test: update occupancy cache performance tests (#25)
    * Changed concurrent users to users per second with ramp up period to simulate more realistic load, where requests are generated even if previous ones are still waiting.
    * Fixed dates used in the simulation.
    * Used more configurations to test system under different loads.
    * Updated README

--- src/test/java/io/github/wojtekolo/hotelsystem/room/api/RoomOccupancySimulation.java
+++ src/test/java/io/github/wojtekolo/hotelsystem/room/api/RoomOccupancySimulation.java
@@ -3,6 +3,8 @@ package io.github.wojtekolo.hotelsystem.room.api;
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
+import java.time.LocalDate;
+
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 import static io.gatling.javaapi.jdbc.JdbcDsl.jdbcFeeder;
@@ -11,7 +13,8 @@ public class RoomOccupancySimulation extends Simulation {
 
     HttpProtocolBuilder httpProtocol = http
             .baseUrl("http://localhost:8080")
-            .acceptHeader("application/json");
+            .acceptHeader("application/json")
+            .shareConnections();
 
     FeederBuilder<Object> roomFeeder = jdbcFeeder(
             "jdbc:postgresql://localhost:5432/hotel_system",
@@ -19,16 +22,16 @@ public class RoomOccupancySimulation extends Simulation {
             "secret",
             "SELECT id AS \"randomRoomId\" FROM room"
     ).random();
-
+    LocalDate now = LocalDate.now();
     ScenarioBuilder scn = scenario("Room Occupancy Cache Test")
             .feed(roomFeeder)
             .exec(http("Get Occupancy Request")
                     .get("/api/v1/rooms/#{randomRoomId}/occupancy")
-                    .queryParam("from", "2026-05-01")
-                    .queryParam("to", "2026-05-31")
+                    .queryParam("from", now.plusDays(2))
+                    .queryParam("to", now.plusDays(20))
                     .check(status().is(200))).pause(1);
 
     {
-        setUp(scn.injectClosed(constantConcurrentUsers(100).during(60))).protocols(httpProtocol);
+        setUp(scn.injectOpen(rampUsersPerSec(1).to(200).during(300))).protocols(httpProtocol);
     }
 }
